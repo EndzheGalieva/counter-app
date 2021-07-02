7c7bc92ce8447650c6ca121fadd2ed9ec327ce68
Endzhe Galieva <endzhe91@gmail.com>
2021-07-02 20:54:31 +0300

    Mounting Phase

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 // Stateless Functional Component
-const NavBar = ({ totalCounters }) => {
+const NavBar = props => {
+  console.log('NavBar - Rendered');
+
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          Navbar <span className="badge badge-pill badge-secondary">{totalCounters}</span>
+          Navbar{" "} <span className="badge badge-pill badge-secondary">{props.totalCounters}</span>
         </a>
       </div>
     </nav>
